d5b18d41be90c3ad71c93625e612a2c772c93657
DanKaplanSES <daniel.kaplan@sleepeasysoftware.com>
2022-09-01 00:34:56 -0700

    formatting

--- menu.js
+++ menu.js
@@ -1,35 +1,35 @@
-(function(){
+(function () {
     var metaKey;
-    
+
     updateMetaKey();
-    
-    chrome.storage.onChanged.addListener(function(changes, areaName){
-        if(areaName == "sync"){
+
+    chrome.storage.onChanged.addListener(function (changes, areaName) {
+        if (areaName == "sync") {
             updateMetaKey();
         }
     })
 
-    document.addEventListener("click", function(event){
-        if((event.altKey && metaKey == "Alt")
+    document.addEventListener("click", function (event) {
+        if ((event.altKey && metaKey == "Alt")
             || (event.ctrlKey && metaKey == "Ctrl")
-            || (event.shiftKey && metaKey == "Shift")) { 
+            || (event.shiftKey && metaKey == "Shift")) {
             copyCommand(event.target);
             event.preventDefault();
         }
     }, false);
 
-    function updateMetaKey(){
+    function updateMetaKey() {
         chrome.storage.sync.get({
             metaKey: 'Alt',
-        }, function(items) {
+        }, function (items) {
             metaKey = items.metaKey;
         });
     };
 
-    function copyCommand(clickedElement){
+    function copyCommand(clickedElement) {
         var text = getText(clickedElement.firstChild, "\r\n");
         copy(text.trim());
-        
+
         var rect = clickedElement.getBoundingClientRect();
         var frame = document.createElement("div");
         Object.assign(frame.style, {
@@ -42,27 +42,27 @@
             borderRadius: "5px",
             zIndex: "99999",
             pointerEvents: "none"
-        }); 
+        });
 
         document.body.appendChild(frame);
-        
+
         $(frame).fadeIn(300, "swing").delay(500).fadeOut(500, "swing");
-        
+
         // console.log(text.trim());
     }
 
-    function copy(text){
+    function copy(text) {
         var textArea = document.createElement("textarea");
         textArea.style.cssText = "position: absolute; left: -100%;";
 
-        try{
+        try {
             document.body.appendChild(textArea);
 
             textArea.value = text;
             textArea.select();
-        
+
             document.execCommand("copy");
-        }finally{
+        } finally {
             document.body.removeChild(textArea);
         }
     }
--- options.js
+++ options.js
@@ -1,15 +1,15 @@
 function saveOptions() {
-  chrome.storage.sync.set({
-    metaKey: document.getElementById("meta_key").value,
-  });
+    chrome.storage.sync.set({
+        metaKey: document.getElementById("meta_key").value,
+    });
 }
 
 function restoreOptions() {
-  chrome.storage.sync.get({
-    metaKey: 'Alt',
-  }, function(items) {
-    document.getElementById("meta_key").value = items.metaKey;
-  });
+    chrome.storage.sync.get({
+        metaKey: 'Alt',
+    }, function (items) {
+        document.getElementById("meta_key").value = items.metaKey;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
